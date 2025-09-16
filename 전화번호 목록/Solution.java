ec005f5953155c9063aec3031f186410a43ce71d
조상준 <94978051+joon0447@users.noreply.github.com>
2025-09-16 10:32:48 +0900

    Create Solution.java

---
+++ 전화번호 목록/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
+        for(int i=0; i<phone_book.length-1; i++){
+            int len = phone_book[i].length();
+            if(phone_book[i+1].length() < len) continue;
+            if(phone_book[i].equals(phone_book[i+1].substring(0,len))) return false;
+        }
+        
+        return true;
+    }
+}
