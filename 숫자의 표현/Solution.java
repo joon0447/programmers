4eef100d2a321c1341484a987f60d4898915d6c6
조상준 <94978051+joon0447@users.noreply.github.com>
2025-09-23 09:57:53 +0900

    Create Solution.java

---
+++ 숫자의 표현/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n; i++){
+            int total = 0;
+            for(int j=i; j<=n; j++){
+                total += j;
+                if(total == n){
+                    answer++;
+                    break;
+                }
+                
+                if(total > n){
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
