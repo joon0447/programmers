9884bed10abc13f56be662ceb77fe3b72f395242
조상준 <94978051+joon0447@users.noreply.github.com>
2025-09-23 10:20:56 +0900

    Create Solution.java

---
+++ 피보나치 수/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1); 
+
+        for (int i = 2; i <= n; i++) {
+            list.add((list.get(list.size() - 1) + list.get(list.size() - 2)) % 1234567);
+        }
+
+        return list.get(list.size() - 1) % 1234567;
+    }
+}
