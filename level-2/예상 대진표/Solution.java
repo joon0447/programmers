ff33a8cf0836b36f7139b5029fc3f726d020f2e8
조상준 <94978051+joon0447@users.noreply.github.com>
2025-09-30 03:23:14 +0000

    예상 대진표

---
+++ level-2/예상 대진표/Solution.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+        a--;
+        b--;
+        
+         while ((a /= 2) != (b /= 2)) answer++;
+        
+        return answer;
+    }
+}
